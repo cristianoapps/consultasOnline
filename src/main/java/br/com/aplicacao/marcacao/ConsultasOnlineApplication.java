e7374777307143cb4ae572b49510ed0461628c3c
Cristiano Seixas <cristianoseixasg100@gmail.com>
2020-07-06 19:43:02 -0300

    Projeto criado

---
+++ src/main/java/br/com/aplicacao/marcacao/ConsultasOnlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.aplicacao.marcacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultasOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultasOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aplicacao/marcacao/ConsultasOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aplicacao.marcacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultasOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

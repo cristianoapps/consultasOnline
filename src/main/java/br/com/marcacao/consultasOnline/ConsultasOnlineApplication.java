7a23844bba72438d82f4e5db1902fa71145fa512
Cristiano Seixas <cristianoseixasg100@gmail.com>
2020-07-06 20:29:20 -0300

    Testando a chamada do REST - Test ok

--- src/main/java/br/com/marcacao/consultasOnline/ConsultasOnlineApplication.java
+++ src/main/java/br/com/marcacao/consultasOnline/ConsultasOnlineApplication.java
@@ -1,4 +1,4 @@
-package br.com.aplicacao.marcacao;
+package br.com.marcacao.consultasOnline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
